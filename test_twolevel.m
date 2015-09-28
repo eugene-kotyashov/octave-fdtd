f1ae1fd144a1f008b2a077a94f5ba85be9c46de6
eugene <eugene.kotyashov@gmail.com>
2015-09-28 18:32:46 +0300

    now it works with polarization added

--- test_twolevel.m
+++ test_twolevel.m
@@ -5,8 +5,9 @@ qe = physical_constant("elementary charge");
 me = physical_constant("electron mass");
 eps0 = physical_constant("electric constant");
 hbar = physical_constant("Planck constant")/(2*pi);
-delt = 1;
-delz = 2*c0*delt;
+eta0 = physical_constant("characteristic impedance of vacuum");
+mu0 = physical_constant("mag. constant");
+
 tsteps=10000;
 zsteps=500;
 eps_yy = ones(1, zsteps);
@@ -23,25 +24,38 @@ zsource = 50;
 %device specification 
 start_device = 100;
 end_device = 300;
-eps_device = 9;
+eps_device = 1;
 eps_yy(start_device:end_device) = eps_device; 
 
 %two level parameters
-omega_a = 2*pi*5e14;
-del_omega_a = 2*pi*5e13;
-delN0=1e26;
-tau21=20e-9;
+omega_a_val = 2*pi*5e14;
+omega_a = zeros(1,zsteps);
+omega_a(start_device:end_device) = omega_a_val;;
+del_omega_a = zeros(1,zsteps);
+del_omega_a(start_device:end_device) = 2*pi*5e13;
+nk1 = zeros(1,zsteps);
+nk1(start_device:end_device) = mu0./(hbar*omega_a(start_device:end_device));
+delN0=zeros(1,zsteps);
+delN0(start_device:end_device) = 1e26;
+tau21 = 20e-9;
+invtau21 = zeros(1,zsteps);
+invtau21(start_device:end_device) =1/tau21;
 gamma_r=1e7;
-gamma_CEO=(qe^2/me)*(omega_a^2/(6*pi*eps0*c0^3));
-kappa = (gamma_r/gamma_CEO)*(qe^2/me);
+gamma_CEO=(qe^2/me)*(omega_a.^2/(6*pi*eps0*c0^3));
+kappa = zeros(1,zsteps);
+kappa(start_device:end_device) = ((1/eps0)*gamma_r./gamma_CEO(start_device:end_device))*(qe^2/me);
+%define simulation steps
+delt = 0.005*(2*pi/omega_a_val);
+delz = 2*c0*delt;
 
-up_hx = ((c0*delt)/delz)./eps_yy;
-up_ey = ((c0*delt)/delz)./mu_xx;
+up_hx = ((c0*delt)/delz)./mu_xx;
+up_dy = (c0*delt)/delz;
 
 src_lambda = 100*delz;
 src_omega = 2*pi*c0/src_lambda;
 src_period = 0.2*src_lambda/c0;
 src_freq = 1/src_period;
+
 max_freq = 0.5*src_freq;
 
 %prepare fourier transform arrays
@@ -55,8 +69,9 @@ norm_src = zeros(1, nfreqs);
 
 time = linspace(0,delt*tsteps, tsteps);
 nsrc = 1;
-Ey_source = exp(-((time-6*src_period)/(src_period)).^2);
-Hx_source = -sqrt(eps_yy(1,zsource)/mu_xx(1,zsource)).*exp(-(((time+0.5*delz*nsrc/c0+0.5*delt)-6*src_period)/(src_period)).^2);
+Ey0=1e5;
+Ey_source = Ey0*exp(-((time-6*src_period)/(src_period)).^2);
+Hx_source = -Ey0*sqrt(eps_yy(1,zsource)/mu_xx(1,zsource)).*exp(-(((time+0.5*delz*nsrc/c0+0.5*delt)-6*src_period)/(src_period)).^2);
 N=0;
 Nspace = 100;
 
@@ -80,26 +95,27 @@ for t=1:tsteps-1
 	Hx(1,zsource-1)-=up_hx(1,zsource-1)*Ey_source(1,t);
 %two level additional equations for polatization and inversion
 	Py_old = Py;	
-	Py = ((del_omega_a*delt-2)*Py_old + 2*delt^2*delN.*Ey + (4-2*(omega_a*delt)^2)*Py)/(del_omega_a*delt+2);
+	Py = ((del_omega_a*delt-2).*Py_old + 2*delt^2*kappa.*delN.*Ey + (4-2*(omega_a*delt).^2).*Py)./(del_omega_a*delt+2);
 	delN_old = delN;
-	delN= - 2*Ey.*Py/(omega_a*hbar) + delN_old+2*Ey.*Py_old/(omega_a*hbar) + 2*delz*(delN0-delN)/tau21;
+	delN= - 2*nk1.*Ey.*(Py-Py_old) + delN_old + 2*delz*(delN0-delN).*invtau21;
 			
 
 %       ey boundary condition
-	Dy(1,1)+=c0.*(Hx(1,1)-h3);
+	Dy(1,1)+=up_dy*(Hx(1,1)-h3);
 
-	Dy(2:zsteps)+=c0.*diff(Hx);
+	Dy(2:zsteps)+=up_dy*diff(Hx);
 
 	e3 = e2;
 	e2 = e1;
 	e1 = Dy(1,zsteps);
 %	TF/SF source correction
-	Dy(1,zsource)-=c0*Hx_source(1,t);
+	Dy(1,zsource)-=up_dy*Hx_source(1,t);
 %	soft source
 %	Ey(1,zsource)+=Ey_source(1,t);
 
 %	calculation Ey from Dy
-	Ey = (Dy-Py)./(eps0*eps_yy);
+	%Ey = Dy./eps_yy;
+	Ey = (Dy-Py)./eps_yy;
 
 	tmp = K.^t;
 	ref+= Ey(1)*tmp;
@@ -112,12 +128,13 @@ for t=1:tsteps-1
 		plot( linspace(1,zsteps,zsteps),Ey,"r",
 			linspace(1,zsteps,zsteps),Hx,"b");
 		title(num2str(t));		
-		ylim([-1.5,1.5],"manual");
+		ylim([-1.5*Ey0,1.5*Ey0],"manual");
 		subplot(2,1,2);
 		plot(freqs,(abs(ref)./abs(norm_src)).^2,"m",
-			freqs,(abs(trans)./abs(norm_src)).^2,"g",
-			freqs, (abs(ref)./abs(norm_src)).^2 + (abs(trans)./abs(norm_src)).^2,"b");		
+			2*pi*freqs,(abs(trans)./abs(norm_src)).^2,"g",
+			2*pi*freqs, (abs(ref)./abs(norm_src)).^2 + (abs(trans)./abs(norm_src)).^2,"b");		
 		title("reflectance and transmittance");		
+		ylim([0,1.2],"manual");
 		pause(0.001);
 		N = 0;		
 	end;
