37ba2f40b5e71e5b2f7d3724ff0b31e101293fb2
eugene <eugene.kotyashov@gmail.com>
2015-10-09 14:41:55 +0300

    fixed polarization and pop. diff equations, Rabi frequincy test with sinusoidal source

--- test_twolevel.m
+++ test_twolevel.m
@@ -8,7 +8,7 @@ hbar = physical_constant("Planck constant")/(2*pi);
 eta0 = physical_constant("characteristic impedance of vacuum");
 mu0 = physical_constant("mag. constant");
 
-tsteps=200;
+tsteps=10000;
 zsteps=500;
 eps_yy = ones(1, zsteps);
 mu_xx = ones(1, zsteps);
@@ -16,8 +16,10 @@ Ey=zeros(1,zsteps);
 Dy=zeros(1,zsteps);
 Py=zeros(1,zsteps);
 delN = zeros(1,zsteps);
+Py_old2 = zeros(1,zsteps);
 Py_old = zeros(1,zsteps);
 delN_old = zeros(1,zsteps);
+delN_old2 = zeros(1,zsteps);
 
 Hx=zeros(1,zsteps);
 zsource = 50;
@@ -38,6 +40,7 @@ nk1(start_device:end_device) = mu0./(hbar*omega_a(start_device:end_device));
 delN0_val = 1e26;
 delN0=zeros(1,zsteps);
 delN0(start_device:end_device) = delN0_val;
+delN = delN0;
 tau21 = 20e-9;
 invtau21 = zeros(1,zsteps);
 invtau21(start_device:end_device) =1/tau21;
@@ -46,16 +49,13 @@ gamma_CEO=(qe^2/me)*(omega_a.^2/(6*pi*eps0*c0^3));
 kappa = zeros(1,zsteps);
 kappa(start_device:end_device) = ((1/eps0)*gamma_r./gamma_CEO(start_device:end_device))*(qe^2/me);
 %define simulation steps
-delt = 0.005*(2*pi/omega_a_val);
+delt = 0.001*(2*pi/omega_a_val);
 delz = 2*c0*delt;
 
 up_hx = ((c0*delt)/delz)./mu_xx;
 up_dy = (c0*delt)/delz;
 
-src_lambda = 1000*delz;
-src_omega = 2*pi*c0/src_lambda;
-src_period = 0.2*src_lambda/c0;
-src_freq = 1/src_period;
+
 
 max_freq = 2*omega_a_val/(2*pi);
 
@@ -66,19 +66,30 @@ K = exp(-1i*2*pi*delt*freqs);
 ref = zeros(1, nfreqs);
 trans = zeros(1, nfreqs);
 norm_src = zeros(1, nfreqs);
+delN_ft = zeros(1, nfreqs);
 
 
 time = linspace(0,delt*tsteps, tsteps);
 nsrc = 1;
-Ey0=1e7;
-Ey_source = Ey0*exp(-((time-6*src_period)/(src_period)).^2).*sin(omega_a_val*time);
-Hx_source = -Ey0*sqrt(eps_yy(1,zsource)/mu_xx(1,zsource)).*exp(-(((time+0.5*delz*nsrc/c0+0.5*delt)-6*src_period)/(src_period)).^2).*sin(omega_a_val*time);
+src_lambda = 1000*delz;
+src_omega = 2*pi*c0/src_lambda;
+src_period = 0.2*src_lambda/c0;
+src_freq = 1/src_period;
+
+Ey0=2e7;
+%exp(-((time-6*src_period)/(src_period)).^2).*
+Ey_source = Ey0*sin(omega_a_val*time);
+%exp(-(((time+0.5*delz*nsrc/c0+0.5*delt)-6*src_period)/(src_period)).^2).*
+Hx_source = -Ey0*sqrt(eps_yy(1,zsource)/mu_xx(1,zsource)).*sin(omega_a_val*(time+0.5*delz*nsrc/c0+0.5*delt));
 N=0;
 Nspace = 100;
 
 close all;
 figure;
 hax = gca();
+
+omega_p = (delN0_val*qe^2)/(eps0*me);
+%Chi=abs((2*pi*freqs/c0)*sqrt(1+));
 
 h1 = h2 = h3 = 0;
 e1 = e2 = e3 = 0;
@@ -95,10 +106,12 @@ for t=1:tsteps-1
 %	TF/SF source correction
 	Hx(1,zsource-1)-=up_hx(1,zsource-1)*Ey_source(1,t);
 %two level additional equations for polatization and inversion
-	Py_old = Py;	
-	Py = ((del_omega_a*delt-2).*Py_old + 2*delt^2*kappa.*delN.*Ey + (4-2*(omega_a*delt).^2).*Py)./(del_omega_a*delt+2);
+	Py_old2 = Py_old;	
+	Py_old = Py;
+	Py = ((del_omega_a*delt-2).*Py_old2 + 2*delt^2*kappa.*delN.*Ey + (4-2*(omega_a*delt).^2).*Py)./(del_omega_a*delt+2);
+	delN_old2 = delN_old;
 	delN_old = delN;
-	delN= - 2*nk1.*Ey.*(Py-Py_old) + delN_old + 2*delt*(delN0-delN).*invtau21;
+	delN= - 2*nk1.*Ey.*(Py-Py_old2) + delN_old2 + 2*delt*(delN0-delN).*invtau21;
 			
 
 %       ey boundary condition
@@ -121,7 +134,10 @@ for t=1:tsteps-1
 	tmp = K.^t;
 	ref+= Ey(1)*tmp;
 	trans+=Ey(zsteps)*tmp;
-	norm_src+=Ey_source(t)*tmp;		
+	norm_src+=Ey_source(t)*tmp;
+	if t>1000
+		delN_ft+=delN(ceil(0.5*(start_device+end_device)))*tmp/delN0_val;
+	end;
 
 
 	if N>Nspace
@@ -132,15 +148,19 @@ for t=1:tsteps-1
 		legend("Ey", "Hz");		
 		ylim([-1.5*Ey0,1.5*Ey0],"manual");
 		subplot(3,1,2);
-		plot( linspace(1,zsteps,zsteps),delN,"r");%-delN0)/delN0_val,"r");
+		plot( linspace(1,zsteps,zsteps),delN,"r");
 		title("population difference");		
 		subplot(3,1,3);
-		plot(2*pi*freqs,(abs(ref)./abs(norm_src)).^2,"m",
-			2*pi*freqs,(abs(trans)./abs(norm_src)).^2,"g",
-			2*pi*freqs, (abs(ref)./abs(norm_src)).^2 + (abs(trans)./abs(norm_src)).^2,"b");		
+		plot(
+			2*pi*freqs,abs(delN_ft).^2,"m"			
+			%2*pi*freqs,(abs(ref)./abs(norm_src)).^2,"m",
+			%2*pi*freqs,(abs(trans)./abs(norm_src)).^2,"g",
+			%2*pi*freqs, (abs(ref)./abs(norm_src)).^2 + (abs(trans)./abs(norm_src)).^2,"b"
+		);		
 		title("reflectance and transmittance");
-		legend("Ref", "Trans", "Tot");
-		ylim([0,1.2],"manual");
+		legend("popul. diff. FT:");
+		%legend("Ref", "Trans", "Tot");
+		%ylim([0,1.2],"manual");
 		pause(0.001);
 		N = 0;		
 	end;
