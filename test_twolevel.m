bad35317a1f96ff3ee1e497f695907ec5370211a
eugene <eugene.kotyashov@gmail.com>
2015-11-02 18:59:23 +0300

    TMM calculations added

--- test_twolevel.m
+++ test_twolevel.m
@@ -8,7 +8,7 @@ hbar = physical_constant("Planck constant")/(2*pi);
 eta0 = physical_constant("characteristic impedance of vacuum");
 mu0 = physical_constant("mag. constant");
 
-tsteps=20000;
+tsteps=10;
 zsteps=2000;
 eps_yy = ones(1, zsteps);
 mu_xx = ones(1, zsteps);
@@ -27,15 +27,16 @@ zsource = 50;
 %device specification 
 start_device = 100;
 end_device = 1800;
-eps_device = 1;
+eps_device = 4;
 eps_yy(start_device:end_device) = eps_device; 
 
 %two level parameters
 omega_a_val = 2*pi*5e14;
 omega_a = zeros(1,zsteps);
-omega_a(start_device:end_device) = omega_a_val;;
+omega_a(start_device:end_device) = omega_a_val;
+del_omega_a_val = 2*pi*5e13;
 del_omega_a = zeros(1,zsteps);
-del_omega_a(start_device:end_device) = 2*pi*5e13;
+del_omega_a(start_device:end_device) = del_omega_a_val;
 nk1 = zeros(1,zsteps);
 nk1(start_device:end_device) = mu0./(hbar*omega_a(start_device:end_device));
 delN0_val = 1e26;
@@ -47,8 +48,11 @@ invtau21 = zeros(1,zsteps);
 invtau21(start_device:end_device) =1/tau21;
 gamma_r=1e7;
 gamma_CEO=(qe^2/me)*(omega_a.^2/(6*pi*eps0*c0^3));
+gamma_CEO_val=(qe^2/me)*(omega_a_val^2/(6*pi*eps0*c0^3));
 kappa = zeros(1,zsteps);
+kappa_val = ((1/eps0)*gamma_r/gamma_CEO_val)*(qe^2/me);
 kappa(start_device:end_device) = ((1/eps0)*gamma_r./gamma_CEO(start_device:end_device))*(qe^2/me);
+plasma_freq_val = 2*pi*sqrt(kappa_val*delN0_val);
 
 
 
@@ -80,6 +84,7 @@ max_freq = 2*omega_a_val/(2*pi);
 %prepare fourier transform arrays
 nfreqs = 400;
 freqs = linspace(0,max_freq,nfreqs);
+omegas = 2*pi*freqs;
 K = exp(-1i*2*pi*delt*freqs);
 ref = zeros(1, nfreqs);
 trans = zeros(1, nfreqs);
@@ -95,7 +100,7 @@ src_omega = 2*pi*c0/src_lambda;
 src_period = 0.2*src_lambda/c0;
 src_freq = 1/src_period;
 
-Ey0=1e7;
+Ey0=1e2;
 %exp(-((time-6*src_period)/(src_period)).^2).*
 Ey_source = Ey0*exp(-((time-6*src_period)/(src_period)).^2).*sin(omega_a_val*time);
 %exp(-(((time+0.5*delz*nsrc/c0+0.5*delt)-6*src_period)/(src_period)).^2).*
@@ -107,8 +112,51 @@ close all;
 figure;
 hax = gca();
 
-omega_p = (delN0_val*qe^2)/(eps0*me);
-%Chi=abs((2*pi*freqs/c0)*sqrt(1+));
+plasma_freq_val = 2*pi*sqrt(kappa_val*delN0_val);
+
+
+% ------------------- calculation of reflection coefficients
+%eps_w = zeros(1,nfreqs)+6;
+eps_w = 1+plasma_freq_val^2./(omega_a_val^2 - omegas.^2 - I*del_omega_a_val*omegas);
+ref_w = zeros(1,nfreqs);
+trans_w = zeros(1,nfreqs);
+e_src=[0; 1];
+ey_trans = [0; 0];
+ey_ref = [0; 0];
+L = 20/(2*pi*omega_a_val/c0);
+S0_11 = zeros(2);
+S0_12 = eye(2);
+S0_21 = S0_12;
+S0_22 = S0_11;
+for i_w = 1:nfreqs
+	k_0 = omegas(i_w)/c0;
+	k_z = sqrt(eps_w(i_w));
+	Q=[0, eps_w(i_w); -eps_w(i_w),0];
+	OMEGA = I*k_z*eye(2);
+	Qh = [0 1; -1 0];
+	Vh = -I*Qh;
+	V = Q*(OMEGA^(-1));
+	X=expm(OMEGA*k_0*L);
+	A=eye(2)+V^(-1)*Vh;
+	B=eye(2)-V^(-1)*Vh;
+	D=A-X*B*(A^(-1))*X*B;
+	S11=(D^(-1))*(X*B*(A^(-1))*X*A - B);
+	S22=S11;
+	S12=(D^(-1))*X*(A-B*(A*(-1))*B);
+	S21=S12;
+	S = redheffer(S0_11,S0_12,S0_21,S0_22,S11,S12,S21,S22);
+	S11=[S(1,1) S(1,2); S(2,1) S(2,2)];
+	S12=[S(3,1) S(3,2); S(4,1) S(4,2)];
+	S21=[S(5,1) S(5,2); S(6,1) S(6,2)];
+	S22=[S(7,1), S(7,2); S(8,1) S(8,2)];
+	ey_ref = S11*e_src;
+	ey_trans = S21*e_src;
+	ref_w(i_w)=abs(ey_ref(2))^2;
+	trans_w(i_w)=abs(ey_trans(2))^2;
+	
+end
+%-------------------------------------------
+
 
 h1 = h2 = h3 = 0;
 e1 = e2 = e3 = 0;
@@ -190,9 +238,15 @@ for t=1:tsteps-1
 	end;
 	N++;
 end
+
+figure;
+plot(omegas,ref_w,"g",omegas,trans_w,"r", omegas,(ref_w+trans_w),"b");
 toc;
 %ref=ref*delt;
 %trans=trans*delt;
 %norm_src=norm_src*delt;
+
+
+
 
 
