8cecd35e2126f9c04e3b0727b6635cd5cd976523
eugene <eugene.kotyashov@gmail.com>
2015-12-14 17:39:03 +0300

    fixed many bugs

--- test_simple.c
+++ test_simple.c
@@ -5,11 +5,13 @@
 
 void main()
 {
+
+
 double pi = M_PI;
 double c0=3e8;
 double delt = 1;
 double delz = 2*c0*delt;
-int tsteps=100000;
+int tsteps=20000;
 int zsteps=500;
 
 double* eps_yy = (double*)malloc(zsteps*sizeof(double));
@@ -60,14 +62,14 @@ double max_freq = 0.5*src_freq;
 
 //prepare fourier transform arrays
 int nfreqs = 400;
-double complex * K = (double complex*)malloc(nfreqs*sizeof(double complex));
-double complex * ref = (double complex*)malloc(nfreqs*sizeof(double complex));
-double complex * trans = (double complex*)malloc(nfreqs*sizeof(double complex));
-double complex * norm_src = (double complex*)malloc(nfreqs*sizeof(double complex));
+complex double * K = (complex double*)malloc(nfreqs*sizeof(complex double));
+complex double * ref = (complex double*)malloc(nfreqs*sizeof(complex double));
+complex double * trans = (complex double*)malloc(nfreqs*sizeof(complex double));
+complex double * norm_src = (complex double*)malloc(nfreqs*sizeof(complex double));
 double del_freq = max_freq/nfreqs;
 for(iz=0;iz<nfreqs; iz++)
 {
-	K[iz] = exp(I*2*pi*delt*del_freq*iz);
+	K[iz] = cexp(-I*2*pi*delt*del_freq*iz);
 	ref[iz] = 0;
 	trans[iz] = 0;
 	norm_src[iz] = 0;
@@ -75,6 +77,7 @@ for(iz=0;iz<nfreqs; iz++)
 
 
 double *Ey_source = (double*)malloc(tsteps*sizeof(double));
+double *Ey_time = (double*)malloc(tsteps*sizeof(double));
 
 double *Hx_source = (double*)malloc(tsteps*sizeof(double));
 double time = 0;
@@ -98,14 +101,18 @@ double e1 = 0;
 double e2 = 0;
 double e3 = 0;
 
+#ifdef GNUPLOT_PIPING
+FILE * gnuplotPipe = popen ("gnuplot ", "w");
+#endif
+
 int t;
 for ( t=0; t<tsteps; t++)
 {
 
-	
+
 	for(iz=0;iz<zsteps-1;iz++)
 	{	
-		Hx[iz]+=up_hx[iz]*Ey[iz+1]-Ey[iz];			
+		Hx[iz]+=up_hx[iz]*(Ey[iz+1]-Ey[iz]);			
 	}
 
 //	hx boundary condition
@@ -114,9 +121,10 @@ for ( t=0; t<tsteps; t++)
 	h2 = h1;
 	h1 = Hx[0];
 //	TF/SF source correction
-	Hx[zsource-1]-=up_hx[zsource-1]*Ey_source[0];
+	Hx[zsource-1]-=up_hx[zsource-1]*Ey_source[t];
 //      ey boundary condition
 	Ey[0]+=up_ey[0]*(Hx[0]-h3);
+
 
 	for(iz=1;iz<zsteps;iz++)
 	{
@@ -125,40 +133,68 @@ for ( t=0; t<tsteps; t++)
 
 	e3 = e2;
 	e2 = e1;
-	e1 = Ey[0];
+	e1 = Ey[zsteps-1];
 //	TF/SF source correction
 	Ey[zsource]-=up_ey[zsource]*Hx_source[t];
 //	soft source
 //	Ey(1,zsource)+=Ey_source(1,t);
-	double tmp = 0;
+	complex double tmp;
 	for(iz=0;iz<nfreqs; iz++){
-		tmp = pow(K[iz],t);
+		tmp = cpow(K[iz],t);
 		ref[iz]+= Ey[0]*tmp;
 		trans[iz]+=Ey[zsteps-1]*tmp;
 		norm_src[iz]+=Ey_source[t]*tmp;		
 	}
-/*
+	Ey_time[t] = Ey[zsteps-1];
 
-	if N>Nspace
-		subplot(2,1,1);		
-		plot( linspace(1,zsteps,zsteps),Ey,"r",
-			linspace(1,zsteps,zsteps),Hx,"b");
-		title(num2str(t));		
-		ylim([-1.5,1.5],"manual");
-		subplot(2,1,2);
-		plot(freqs,(abs(ref)./abs(norm_src)).^2,"m",
-			freqs,(abs(trans)./abs(norm_src)).^2,"g",
-			freqs, (abs(ref)./abs(norm_src)).^2 + (abs(trans)./abs(norm_src)).^2,"b");		
-		title("reflectance and transmittance");		
-		pause(0.001);
+#ifdef GNUPLOT_PIPING
+	if (N>Nspace)
+	{			
+		fprintf(gnuplotPipe,"plot '-' with lines\n");
+		for(iz=0;iz<zsteps;iz++)
+		{
+			fprintf(gnuplotPipe, "%d %e\n",
+				iz,
+				Ey[iz]
+			);
+		}
+		fprintf(gnuplotPipe,"e\n");
 		N = 0;		
-	end;
+		usleep(1000000);
+		
+	}
 	N++;
+#endif
+
 }
 
-
-*/
+FILE* fout = fopen("out.txt","w");
+for(iz=0;iz<nfreqs; iz++)
+{
+	fprintf(fout,"%e %e %e %e\n",
+		2*pi*del_freq*iz,
+		cabs(norm_src[iz]),
+		cabs(ref[iz])*cabs(ref[iz])/(cabs(norm_src[iz])*cabs(norm_src[iz])),
+		cabs(trans[iz])*cabs(trans[iz])/(cabs(norm_src[iz])*cabs(norm_src[iz]))
+	);
 }
+fclose(fout);
+
+fout = fopen("time_out.txt", "w");
+for(iz=0;iz<tsteps;iz++)
+{
+	fprintf(fout,"%e %e %e\n",
+		iz*delt,
+		Ey_time[iz],
+		Ey_source[iz]
+	);	
+}
+fclose(fout);
+
+#ifdef GNUPLOT_PIPING
+pclose(gnuplotPipe);
+#endif
+
 free(eps_yy);
 free(mu_xx);
 free(Ey);
