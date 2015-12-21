b3d2ab0a9482e1d44fa8d5808d749ac129ff247a
eugene <eugene.kotyashov@gmail.com>
2015-12-21 18:49:48 +0300

    now threading is much faster

--- test_simple.c
+++ test_simple.c
@@ -5,47 +5,176 @@
 #include <pthread.h>
 
 
-#define NUM_THREADS 4
+#define NUM_THREADS 2
 
+
+pthread_barrier_t time_step_barrier;
 
 typedef struct tag_chunk_info_t
 {
 	int 	tid;	
-	double * Ey;
-	double * up_hx;
-	double * up_ey;
-	double * Hx;
-	int 	z_start;
-	int	z_end;
-	
 } chunk_info_t;
 
-void* updateHx(void* input)
-{		
-	chunk_info_t * info = (chunk_info_t*)input;
-	if (info == NULL) pthread_exit(NULL);
-	int iz;
-	for(iz=info->z_start;iz<=info->z_end;iz++)
-	{	
-		info->Hx[iz]+=info->up_hx[iz]*(info->Ey[iz+1]-info->Ey[iz]);			
-	}
-	pthread_exit(NULL);
-}
 
-void* updateEy(void* input)
+
+double * Ey;
+double * up_hx;
+double * up_ey;
+double * Hx;
+double * Ey_source;
+double * Hx_source;
+int	tsteps;
+int	zsteps;
+int	zsource;
+
+double *Ey_source;
+double *Ey_time;
+
+double *Hx_source;
+
+int nfreqs;
+complex double * K;
+complex double * ref;
+complex double * trans;
+complex double * norm_src;
+
+double N=0;
+double Nspace = 100;
+
+
+double h1 = 0;
+double h2 = 0;
+double h3 = 0;
+double e1 = 0;
+double e2 = 0;
+double e3 = 0;
+
+
+#ifdef GNUPLOT_PIPING
+FILE * gnuplotPipe;
+#endif
+
+
+void * do_time_step(void * input)
 {
-	chunk_info_t * info = (chunk_info_t*)input;
-	if (info == NULL) pthread_exit(NULL);
+
+chunk_info_t * info = (chunk_info_t*)input;
+if (info == NULL) pthread_exit(NULL);
+
+int freq_start = nfreqs*info->tid/NUM_THREADS;
+int freq_end = nfreqs*(info->tid+1)/NUM_THREADS-1;
+
+//for Hx update iz runs from 0 to zsteps-2 - total of zsteps -1 elements to update
+int z_start_hx = (zsteps - 1)*info->tid/NUM_THREADS;
+int z_end_hx = (zsteps - 1)*(info->tid+1)/NUM_THREADS-1;	
+//for Ey update iz runs from 1 to zsteps-1 - total of zsteps -1 elements to update
+int z_start_ey = z_start_hx + 1;
+int z_end_ey = z_end_hx + 1;
+
+int do_ey_source = (zsource <= z_end_ey)&&(zsource >= z_start_ey);
+int do_hx_source = ( (zsource -1) <= z_end_hx)&&( (zsource-1) >= z_start_hx);
+
+if (info->tid == NUM_THREADS-1)
+{
+	z_end_hx = zsteps-2;
+	z_end_ey = zsteps-1;
+}
+
+int t;
+for ( t=0; t<tsteps; t++)
+{
+	
 	int iz;
-	for(iz=info->z_start;iz<=info->z_end;iz++)
-	{
-		info->Ey[iz]+=info->up_ey[iz]*(info->Hx[iz]-info->Hx[iz-1]);
+//========update Hx========================
+	for(iz=z_start_hx;iz<=z_end_hx;iz++)
+	{	
+		Hx[iz]+=up_hx[iz]*(Ey[iz+1]-Ey[iz]);			
 	}
-	pthread_exit(NULL);
+//	hx boundary condition - right boundary
+	if (z_end_hx == zsteps-2)
+	{
+		Hx[zsteps-1]+=up_hx[zsteps-1]*(e3-Ey[zsteps-1]);
+	}
+//collect data needed for ey boundary condition
+	if (z_start_hx == 0)
+	{
+		h3 = h2;
+		h2 = h1;
+		h1 = Hx[0];
+	}
+//	TF/SF source correction		
+	if (do_hx_source)
+	{
+		Hx[zsource-1] -= up_hx[zsource-1]*Ey_source[t];
+	}
+//wait for all threads done with Hx
+	pthread_barrier_wait(&time_step_barrier);
+
+//===========update Ey=====================
+//      ey boundary condition
+	if (z_start_ey == 1)
+	{
+		Ey[0]+=up_ey[0]*(Hx[0]-h3);
+	}
+
+	for(iz=z_start_ey;iz<=z_end_ey;iz++)
+	{
+		Ey[iz]+=up_ey[iz]*(Hx[iz]-Hx[iz-1]);
+	}
+//collect data needed for ey boundary condition
+	if(z_end_ey == zsteps-1)
+	{
+		e3 = e2;
+		e2 = e1;
+		e1 = Ey[zsteps-1];
+	}
+//	TF/SF source correction
+	if (do_ey_source)
+	{
+		Ey[zsource]-=up_ey[zsource]*Hx_source[t];
+	}
+//wait all threads done wuth Ey
+	pthread_barrier_wait(&time_step_barrier);
+
+//perform Fourier transform
+
+	complex double tmp;
+	for(iz=freq_start;iz<=freq_end; iz++){
+		tmp = cpow(K[iz],t);
+		ref[iz]+= Ey[0]*tmp;
+		trans[iz]+=Ey[zsteps-1]*tmp;
+		norm_src[iz]+=Ey_source[t]*tmp;		
+	}
+	if (info->tid == 0)
+		Ey_time[t] = Ey[zsteps-1];
+
+#ifdef GNUPLOT_PIPING
+	if ( (N>Nspace)&&(info->tid == 0) )
+	{			
+		fprintf(gnuplotPipe,"plot '-' with lines\n");
+		for(iz=0;iz<zsteps;iz++)
+		{
+			fprintf(gnuplotPipe, "%d %e\n",
+				iz,
+				Ey[iz]
+			);
+		}
+		fprintf(gnuplotPipe,"e\n");
+		N = 0;		
+		usleep(1000000);
+		
+	}
+	N++;
+#endif
+
+}
+
+pthread_exit(NULL);
+
 }
 
 
-void main()
+int main()
 {
 
 
@@ -53,15 +182,16 @@ double pi = M_PI;
 double c0=3e8;
 double delt = 1;
 double delz = 2*c0*delt;
-int tsteps=20000;
-int zsteps=500;
+
+tsteps=20000;
+zsteps=500;
 
 double* eps_yy = (double*)malloc(zsteps*sizeof(double));
 double* mu_xx = (double*)malloc(zsteps*sizeof(double));
-double* Ey=(double*)malloc(zsteps*sizeof(double));
-double* Hx=(double*)malloc(zsteps*sizeof(double));
-double* up_hx = (double*)malloc(zsteps*sizeof(double));
-double* up_ey = (double*)malloc(zsteps*sizeof(double));
+Ey=(double*)malloc(zsteps*sizeof(double));
+Hx=(double*)malloc(zsteps*sizeof(double));
+up_hx = (double*)malloc(zsteps*sizeof(double));
+up_ey = (double*)malloc(zsteps*sizeof(double));
 double k_grid = (c0*delt)/delz;
 
 
@@ -95,7 +225,7 @@ for(iz=0;iz<zsteps;iz++)
 }
 
 //calculate source
-int zsource = 50;
+zsource = 50;
 double src_lambda = 100*delz;
 double src_omega = 2*pi*c0/src_lambda;
 double src_period = 0.2*src_lambda/c0;
@@ -103,11 +233,12 @@ double src_freq = 1/src_period;
 double max_freq = 0.5*src_freq;
 
 //prepare fourier transform arrays
-int nfreqs = 400;
-complex double * K = (complex double*)malloc(nfreqs*sizeof(complex double));
-complex double * ref = (complex double*)malloc(nfreqs*sizeof(complex double));
-complex double * trans = (complex double*)malloc(nfreqs*sizeof(complex double));
-complex double * norm_src = (complex double*)malloc(nfreqs*sizeof(complex double));
+nfreqs = 400;
+K = (complex double*)malloc(nfreqs*sizeof(complex double));
+ref = (complex double*)malloc(nfreqs*sizeof(complex double));
+trans = (complex double*)malloc(nfreqs*sizeof(complex double));
+norm_src = (complex double*)malloc(nfreqs*sizeof(complex double));
+
 double del_freq = max_freq/nfreqs;
 for(iz=0;iz<nfreqs; iz++)
 {
@@ -118,10 +249,10 @@ for(iz=0;iz<nfreqs; iz++)
 }
 
 
-double *Ey_source = (double*)malloc(tsteps*sizeof(double));
-double *Ey_time = (double*)malloc(tsteps*sizeof(double));
+Ey_source = (double*)malloc(tsteps*sizeof(double));
+Ey_time = (double*)malloc(tsteps*sizeof(double));
 
-double *Hx_source = (double*)malloc(tsteps*sizeof(double));
+Hx_source = (double*)malloc(tsteps*sizeof(double));
 double time = 0;
 double nsrc = sqrt(eps_yy[zsource]*mu_xx[zsource]);
 
@@ -132,19 +263,10 @@ for(iz=0;iz<tsteps;iz++)
 	Hx_source[iz] = -sqrt(eps_yy[zsource]/mu_xx[zsource])*exp(-((time+0.5*delz*nsrc/c0+0.5*delt)-6*src_period)/(src_period)*((time+0.5*delz*nsrc/c0+0.5*delt)-6*src_period)/(src_period));
 }
 
-double N=0;
-double Nspace = 100;
 
-
-double h1 = 0;
-double h2 = 0;
-double h3 = 0;
-double e1 = 0;
-double e2 = 0;
-double e3 = 0;
 
 #ifdef GNUPLOT_PIPING
-FILE * gnuplotPipe = popen ("gnuplot ", "w");
+gnuplotPipe = popen ("gnuplot ", "w");
 #endif
 
 int t;
@@ -155,101 +277,39 @@ pthread_attr_init(&attr);
 pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
 int rc;
 void* status;
-chunk_info_t hx_info[NUM_THREADS];
-chunk_info_t ey_info[NUM_THREADS];
+chunk_info_t info[NUM_THREADS];
+
+pthread_barrier_init(&time_step_barrier,NULL,NUM_THREADS);
+
 for(iz=0;iz<NUM_THREADS;iz++) 
 {
-	hx_info[iz].Ey = Ey;
-	ey_info[iz].Ey = Ey;
-	hx_info[iz].Hx = Hx;
-	ey_info[iz].Hx = Hx;
-	hx_info[iz].up_ey = up_ey;
-	ey_info[iz].up_ey = up_ey;
-	hx_info[iz].up_hx = up_hx;
-	ey_info[iz].up_hx = up_hx;
-	hx_info[iz].tid = iz;
-//for Hx update iz runs from 0 to zsteps-2 - total of zsteps -1 elements to update
-	hx_info[iz].z_start = (zsteps - 1)*iz/NUM_THREADS;
-	hx_info[iz].z_end = (zsteps - 1)*(iz+1)/NUM_THREADS-1;	
-//for Ey update iz runs from 1 to zsteps-1 - total of zsteps -1 elements to update
-	ey_info[iz].z_start = (zsteps - 1)*iz/NUM_THREADS+1;
-	ey_info[iz].z_end = (zsteps - 1)*(iz+1)/NUM_THREADS-1+1;
-	if (iz == NUM_THREADS-1)
+	info[iz].tid = iz;
+	rc = pthread_create(&threads[iz], &attr, do_time_step, (void*)&info[iz]);
+	if (rc) 
 	{
-		hx_info[iz].z_end = zsteps-2;
-		ey_info[iz].z_end = zsteps-1;
-	}
+	        printf("ERROR; return code from pthread_create() is %d\n", rc);
+	        exit(-1);
+        }
 }
 
-for ( t=0; t<tsteps; t++)
+
+for(iz=0;iz<NUM_THREADS;iz++) 
 {
-
-	for(iz=0;iz<NUM_THREADS;iz++)
-	{		
-		rc = pthread_create(&threads[iz],&attr, updateHx, (void*)(&hx_info[iz]));
-	}
-	for(iz=0;iz<NUM_THREADS;iz++)
-	{
-		rc = pthread_join(threads[iz],&status);
-	}
-	
-
-//	hx boundary condition
-	Hx[zsteps-1]+=up_hx[zsteps-1]*(e3-Ey[zsteps-1]);
-	h3 = h2;
-	h2 = h1;
-	h1 = Hx[0];
-//	TF/SF source correction
-	Hx[zsource-1]-=up_hx[zsource-1]*Ey_source[t];
-//      ey boundary condition
-	Ey[0]+=up_ey[0]*(Hx[0]-h3);
-
-	for(iz=0;iz<NUM_THREADS;iz++)
-	{		
-		rc = pthread_create(&threads[iz],&attr, updateEy, (void*)(&ey_info[iz]));
-	}	
-	for(iz=0;iz<NUM_THREADS;iz++)
-	{
-		rc = pthread_join(threads[iz],&status);
-	}
-	
-
-	e3 = e2;
-	e2 = e1;
-	e1 = Ey[zsteps-1];
-//	TF/SF source correction
-	Ey[zsource]-=up_ey[zsource]*Hx_source[t];
-//	soft source
-//	Ey(1,zsource)+=Ey_source(1,t);
-	complex double tmp;
-	for(iz=0;iz<nfreqs; iz++){
-		tmp = cpow(K[iz],t);
-		ref[iz]+= Ey[0]*tmp;
-		trans[iz]+=Ey[zsteps-1]*tmp;
-		norm_src[iz]+=Ey_source[t]*tmp;		
-	}
-	Ey_time[t] = Ey[zsteps-1];
-
-#ifdef GNUPLOT_PIPING
-	if (N>Nspace)
-	{			
-		fprintf(gnuplotPipe,"plot '-' with lines\n");
-		for(iz=0;iz<zsteps;iz++)
-		{
-			fprintf(gnuplotPipe, "%d %e\n",
-				iz,
-				Ey[iz]
-			);
-		}
-		fprintf(gnuplotPipe,"e\n");
-		N = 0;		
-		usleep(1000000);
-		
-	}
-	N++;
-#endif
-
+	info[iz].tid = iz;
+	rc = pthread_join(threads[iz], &status);
+	if (rc) {
+	        printf("ERROR; return code from pthread_join() is %d\n", rc);
+        exit(-1);
+        }
 }
+
+
+
+pthread_attr_destroy(&attr);
+pthread_barrier_destroy(&time_step_barrier);
+
+
+
 
 FILE* fout = fopen("out.txt","w");
 for(iz=0;iz<nfreqs; iz++)
